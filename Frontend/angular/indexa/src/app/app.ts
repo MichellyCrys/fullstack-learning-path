0d98ba21cf2f05014069f5e518499adb6e95ec3f
Michelly Gomes <michelly.gomes@eldorado.org.br>
2026-02-02 11:37:54 -0400

    FormsModule e NgModel(aula 05)

--- Frontend/angular/indexa/src/app/app.ts
+++ Frontend/angular/indexa/src/app/app.ts
@@ -4,6 +4,7 @@ import { Container } from "./componentes/container/container";
 import { Cabecalho } from "./componentes/cabecalho/cabecalho";
 import { Separador } from "./componentes/separador/separador";
 import { Contato } from "./componentes/contato/contato";
+import { FormsModule } from '@angular/forms';
 
 interface ContatoInterface{
   id: number
@@ -20,7 +21,8 @@ import agenda from './agenda.json'
     Container,
     Cabecalho,
     Separador,
-    Contato
+    Contato,
+    FormsModule
 ],
   templateUrl: './app.html',
   styleUrl: './app.css'
@@ -29,9 +31,25 @@ export class App {
   alfabeto: string = 'abcdefghijklmnopqrstuvwxyz';
   contatos: ContatoInterface[] = agenda;
 
+  filtroPorTexto: string = ''
+
+  private removerAcentos(texto: string): string {
+    return texto.normalize('NFD').replace(/[\u0300-\u036f]/g, '');
+  }
+
+  filtrarContatosPorTexto(): ContatoInterface[]{
+    if(!this.filtroPorTexto){
+      return this.contatos
+    }
+    return this.contatos.filter(contato => {
+      return this.removerAcentos(contato.nome).toLowerCase().includes(this.removerAcentos(this.filtroPorTexto).toLowerCase());
+    })
+
+  }
+
   filtrarContatosPorLetraInicial(letra:string): ContatoInterface[]{
-    return this.contatos.filter( contato => {
-      return contato.nome.toLowerCase().startsWith(letra)
+    return this.filtrarContatosPorTexto().filter( contato => {
+      return this.removerAcentos(contato.nome).toLowerCase().startsWith(this.removerAcentos(letra).toLowerCase());
     })
   }
 }
