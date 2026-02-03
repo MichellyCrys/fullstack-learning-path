49724ea78b542dcc6699807b0c494ef938932e0b
Michelly Gomes <michelly.gomes@eldorado.org.br>
2026-02-03 09:17:08 -0400

    feat: validação em forms reativos

--- Frontend/angular/memoteca/src/app/componentes/pensamentos/criar-pensamento/criar-pensamento.ts
+++ Frontend/angular/memoteca/src/app/componentes/pensamentos/criar-pensamento/criar-pensamento.ts
@@ -1,15 +1,16 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 import { Router, RouterLink } from "@angular/router";
 import { InfoPensamento } from '../infoPensamento';
 import { PensamentoService } from '../pensamento.service';
+import { minusculoValidator } from './minusculoValidators';
 
 @Component({
   selector: 'app-criar-pensamento',
   standalone: true,
   imports: [
     FormsModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
 ],
   templateUrl: './criar-pensamento.html',
   styleUrl: './criar-pensamento.css',
@@ -26,16 +27,27 @@ export class CriarPensamento implements OnInit{
 
   ngOnInit(): void {
     this.formulario = this.formBuilder.group({
-      conteudo: ['Formulário reativo'],
-      autoria: ['Angular'],
+      conteudo: ['', Validators.compose([
+        Validators.required,
+        Validators.pattern(/(.|\s)*\S(.|\s)*/)
+      ])],
+      autoria: ['', Validators.compose([
+        Validators.required,
+        Validators.minLength(3),
+        minusculoValidator
+      ])],
       modelo: ['modelo1']
     })
   }
 
   criarPensamento(){
-    this.service.criar(this.formulario.value).subscribe(() =>{
-      this.router.navigate(['/listarPensamento']);
-    })
+    if(this.formulario.valid){
+      this.service.criar(this.formulario.value).subscribe(() =>{
+        this.router.navigate(['/listarPensamento']);
+      })
+    } else {
+      this.formulario.markAllAsTouched();
+    }
   }
 
   cancelarPensamento(){
--- Frontend/angular/memoteca/src/app/componentes/pensamentos/editar-pensamento/editar-pensamento.ts
+++ Frontend/angular/memoteca/src/app/componentes/pensamentos/editar-pensamento/editar-pensamento.ts
@@ -1,42 +1,53 @@
 import { Component, OnInit } from '@angular/core';
 import { InfoPensamento } from '../infoPensamento';
-import { FormsModule } from '@angular/forms';
+import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 import { PensamentoService } from '../pensamento.service';
 import { ActivatedRoute, Router } from "@angular/router";
+import { minusculoValidator } from '../criar-pensamento/minusculoValidators';
 
 @Component({
   selector: 'app-editar-pensamento',
   imports: [
-    FormsModule
+    FormsModule,
+    ReactiveFormsModule
   ],
   templateUrl: './editar-pensamento.html',
   styleUrl: './editar-pensamento.css',
 })
 export class EditarPensamento implements OnInit{
 
-  pensamento: InfoPensamento ={
-    id: '',
-    conteudo: '',
-    autoria: '',
-    modelo: ''
-  }
+  formulario!: FormGroup;
+
   constructor(
     private service: PensamentoService,
     private router: Router,
-    private route: ActivatedRoute
+    private route: ActivatedRoute,
+    private formBuilder: FormBuilder
   ){
   }
 
   ngOnInit(): void {
-    const id = this.route.snapshot.paramMap.get('id');
+    const id = this.route.snapshot.paramMap.get('id')
     this.service.buscarPorId(id!).subscribe((pensamento) => {
-      this.pensamento = pensamento;
+      this.formulario = this.formBuilder.group({
+        id: [pensamento.id],
+        conteudo: [pensamento.conteudo, Validators.compose([
+          Validators.required,
+          Validators.pattern(/(.|\s)*\S(.|\s)*/)
+        ])],
+        autoria: [pensamento.autoria, Validators.compose([
+          Validators.required,
+          Validators.minLength(3),
+          minusculoValidator
+        ])],
+        modelo: [pensamento.modelo]
+      })
     })
   }
 
-  editarPensamento(){
-    this.service.editarPensamento(this.pensamento).subscribe(() =>{
-      this.router.navigate(['/listarPensamento']);
+  editarPensamento() {
+    this.service.editarPensamento(this.formulario.value).subscribe(() => {
+      this.router.navigate(['/listarPensamento'])
     })
   }
 
