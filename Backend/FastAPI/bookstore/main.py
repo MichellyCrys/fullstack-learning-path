b2f044181238d42914a283e38401e650fe1e0c87
Michelly Gomes <michelly.gomes@eldorado.org.br>
2026-02-04 15:11:41 -0400

    usando get

--- Backend/FastAPI/bookstore/main.py
+++ Backend/FastAPI/bookstore/main.py
@@ -2,6 +2,23 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
-@app.get("/books")
-async def root():
-    return {"message": "Hello World"}
+BOOK_DATABASE = [
+    "Harry Potter and the Chamber of Secrets",
+    "Lord of the Rings",
+    "The da Vinci Code"
+]
+
+
+# /           -> boas vindas
+@app.get("/")
+async def home():
+    return "Welcome to my bookstore"
+# /list-books -> listar todos os livros
+@app.get("/list-books")
+async def list_books():
+    return {"books": BOOK_DATABASE}
+
+
+# /list-bools-by-index/{index} -> listar um livro
+# /get-random-book -> livro aleatório
+# /add-book -> adicionar novo livro
