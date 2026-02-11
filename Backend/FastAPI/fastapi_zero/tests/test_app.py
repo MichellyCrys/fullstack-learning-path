63fc1cd8392d974e7984ee744f5529e7e577ec37
Michelly Gomes <michelly.gomes@eldorado.org.br>
2026-02-11 11:40:35 -0400

    Atualizando endpoints para usar o banco de dados real

--- Backend/FastAPI/fastapi_zero/tests/test_app.py
+++ Backend/FastAPI/fastapi_zero/tests/test_app.py
@@ -1,5 +1,7 @@
 from http import HTTPStatus
 
+from fastapi_zero.schemas import UserPublic
+
 
 def test_root_deve_retornar_ok_e_ola_mundo(client):
 
@@ -31,18 +33,16 @@ def test_read_users(client):
     response = client.get('/users/')
 
     assert response.status_code == HTTPStatus.OK
-    assert response.json() == {
-        'users': [
-            {
-                'id': 1,
-                'username': 'alice',
-                'email': 'alice@example.com',
-            }
-        ]
-    }
+    assert response.json() == {'users': []}
 
 
-def test_update_user(client):
+def test_read_users_with_users(client, user):
+    user_schema = UserPublic.model_validate(user).model_dump()
+    response = client.get('/users/')
+    assert response.json() == {'users': [user_schema]}
+
+
+def test_update_user(client, user):
     response = client.put(
         '/users/1',
         json={
@@ -59,15 +59,11 @@ def test_update_user(client):
     }
 
 
-def test_delete_user(client):
+def test_delete_user(client, user):
     response = client.delete('/users/1')
 
     assert response.status_code == HTTPStatus.OK
-    assert response.json() == {
-        'username': 'bob',
-        'email': 'bob@example.com',
-        'id': 1,
-    }
+    assert response.json() == {'message': 'User deleted'}
 
 
 def test_update_user_should_return_not_found__exercicio(client):
@@ -90,28 +86,24 @@ def test_delete_user_should_return_not_found__exercicio(client):
     assert response.json() == {'detail': 'User not found'}
 
 
-def test_get_user_should_return_not_found__exercicio(client):
-    response = client.get('/users/666')
-
-    assert response.status_code == HTTPStatus.NOT_FOUND
-    assert response.json() == {'detail': 'User not found'}
-
-
-def test_get_user___exercicio(client):
+def test_update_integrity_error(client, user):
     client.post(
-        '/users/',
+        '/users',
         json={
-            'username': 'bob',
+            'username': 'fausto',
+            'email': 'fausto@example.com',
+            'password': 'secret',
+        },
+    )
+
+    response = client.put(
+        f'/users/{user.id}',
+        json={
+            'username': 'fausto',
             'email': 'bob@example.com',
             'password': 'mynewpassword',
         },
     )
 
-    response = client.get('/users/1')
-
-    assert response.status_code == HTTPStatus.OK
-    assert response.json() == {
-        'username': 'bob',
-        'email': 'bob@example.com',
-        'id': 1,
-    }
+    assert response.status_code == HTTPStatus.CONFLICT
+    assert response.json() == {'detail': 'Username or Email already exists'}
