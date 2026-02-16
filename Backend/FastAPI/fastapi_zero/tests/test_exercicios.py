18987c058a3425f9058177759027f88fc4c5a56f
Michelly Gomes <michelly.gomes@eldorado.org.br>
2026-02-16 12:05:48 -0400

    refactor: Refatorando estrutura do projeto: Criado routers para Users e Auth; movido constantes para variáveis de ambiente.(Aula 07)

---
+++ Backend/FastAPI/fastapi_zero/tests/test_exercicios.py
@@ -0,0 +1,70 @@
+# Resolução de Exercícios
+from http import HTTPStatus
+
+
+def test_update_integrity_error(client, user, token):
+    client.post(
+        '/users/',
+        json={
+            'username': 'fausto',
+            'email': 'fausto@example.com',
+            'password': 'secret',
+        },
+    )
+
+    response = client.put(
+        f'/users/{user.id}',
+        headers={'Authorization': f'Bearer {token}'},
+        json={
+            'username': 'fausto',
+            'email': 'bob@example.com',
+            'password': 'mynewpassword',
+        },
+    )
+
+    assert response.status_code == HTTPStatus.CONFLICT
+    assert response.json() == {'detail': 'Username or Email already exists'}
+
+
+def test_create_user_should_username_exists__exercicio(client, user):
+    response = client.post(
+        '/users/',
+        json={
+            'username': user.username,
+            'email': 'alice@example.com',
+            'password': 'secret',
+        },
+    )
+    assert response.status_code == HTTPStatus.CONFLICT
+    assert response.json() == {'detail': 'Username already exists'}
+
+
+def test_create_user_should_return_409_email_exists__exercicio(client, user):
+    response = client.post(
+        '/users/',
+        json={
+            'username': 'alice',
+            'email': user.email,
+            'password': 'secret',
+        },
+    )
+    assert response.status_code == HTTPStatus.CONFLICT
+    assert response.json() == {'detail': 'Email already exists'}
+
+
+def test_get_user_should_return_not_found__exercicio(client):
+    response = client.get('/users/666')
+
+    assert response.status_code == HTTPStatus.NOT_FOUND
+    assert response.json() == {'detail': 'User not found'}
+
+
+def test_get_user___exercicio(client, user):
+    response = client.get(f'/users/{user.id}')
+
+    assert response.status_code == HTTPStatus.OK
+    assert response.json() == {
+        'username': user.username,
+        'email': user.email,
+        'id': user.id,
+    }
--- Backend/FastAPI/fastapi_zero/tests/test_security.py
+++ Backend/FastAPI/fastapi_zero/tests/test_security.py
@@ -2,15 +2,15 @@ from http import HTTPStatus
 
 from jwt import decode, encode
 
-from fastapi_zero.security import ALGORITHM, SECRET_KEY, create_access_token
+from fastapi_zero.security import create_access_token
 
 
-def test_jwt():
+def test_jwt(settings):
     data = {'test': 'test'}
 
     token = create_access_token(data)
 
-    decoded = decode(token, SECRET_KEY, algorithms=ALGORITHM)
+    decoded = decode(token, settings.SECRET_KEY, algorithms=settings.ALGORITHM)
 
     assert decoded['test'] == data['test']
     assert 'exp' in decoded
@@ -25,8 +25,12 @@ def test_jwt_invalid_token(client):
     assert response.json() == {'detail': 'Could not validate credentials'}
 
 
-def test_get_current_user_without_sub(client):
-    token = encode({'payload': 'sem email'}, SECRET_KEY, algorithm=ALGORITHM)
+def test_get_current_user_without_sub(client, settings):
+    token = encode(
+        {'payload': 'sem email'},
+        settings.SECRET_KEY,
+        algorithm=settings.ALGORITHM,
+    )
 
     response = client.get(
         '/users/', headers={'Authorization': f'Bearer {token}'}
@@ -36,10 +40,12 @@ def test_get_current_user_without_sub(client):
     assert response.json() == {'detail': 'Could not validate credentials'}
 
 
-def test_get_current_user_not_found(client):
+def test_get_current_user_not_found(client, settings):
     # Token com um email que não está na base
     token = encode(
-        {'sub': 'inexistente@test.com'}, SECRET_KEY, algorithm=ALGORITHM
+        {'sub': 'inexistente@test.com'},
+        settings.SECRET_KEY,
+        algorithm=settings.ALGORITHM,
     )
 
     response = client.get(
