d37fee4b89d3be9631b26d830501f9c85d70ddd9
Michelly Gomes <michelly.gomes@eldorado.org.br>
2026-03-16 16:57:36 -0400

    feat: Interceptor e guarda de rotas

--- Frontend/angular/jornada-milhas_API_FRONTEND/jornada-milhas/src/app/app.module.ts
+++ Frontend/angular/jornada-milhas_API_FRONTEND/jornada-milhas/src/app/app.module.ts
@@ -27,7 +27,7 @@ import { ModalComponent } from './shared/modal/modal.component';
 import { MatDialogModule } from '@angular/material/dialog';
 import { MatAutocompleteModule } from '@angular/material/autocomplete';
 import { BotaoControleComponent } from './shared/botao-controle/botao-controle.component';
-import { HttpClientModule } from '@angular/common/http';
+import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
 import { PromocoesComponent } from './pages/home/promocoes/promocoes.component';
 import { ReactiveFormsModule } from '@angular/forms';
 import { DropdownUfComponent } from './shared/dropdown-uf/dropdown-uf.component';
@@ -40,6 +40,8 @@ import { MatDividerModule } from '@angular/material/divider';
 import { MatCheckboxModule } from '@angular/material/checkbox';
 import { CadastroComponent } from './pages/cadastro/cadastro.component';
 import { PerfilComponent } from './pages/perfil/perfil.component';
+import { AutenticacaoInterceptor } from './core/interceptors/autenticacao.interceptor';
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -85,7 +87,11 @@ import { PerfilComponent } from './pages/perfil/perfil.component';
     MatDividerModule,
     MatCheckboxModule
   ],
-  providers: [],
+  providers: [{
+    provide: HTTP_INTERCEPTORS,
+    useClass: AutenticacaoInterceptor,
+    multi: true
+}],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
